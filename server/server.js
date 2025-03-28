807985225fc94bca36c0fac8fc87f530b186f761
cris <2022371192@uteq.edu.mx>
2025-03-28 11:55:09 -0600

    Back terminado

--- server/server.js
+++ server/server.js
@@ -1,26 +1,29 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import userRoutes from '../Controllers/userRoutes.js';
-import recipeRoutes from '../Controllers/recipeRoutes.js';
+ import recipeRoutes from '../Controllers/recipeRoutes.js';
 import contactRoutes from '../Controllers/contactRoutes.js';
+import passwordRoutes from '../Controllers/passwordRoutes.js';
+  import favoritesRoutes from '../Controllers/favoritesRoutes.js';
+
+// Cargar variables de entorno
+dotenv.config();
 
 const server = express();
-server.use(bodyParser.json({ limit: '50mb' })); // Aumentar el límite de tamaño de carga útil
-server.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); // Aumentar el límite de tamaño de carga útil para datos codificados en URL
-server.use(cors()); // Usar el middleware de CORS
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); 
+server.use(cors()); 
 
-// Rutas de la API de usuarios
+// Rutas de la API
 server.use('/api/users', userRoutes);
-
-// Rutas de la API de recetas
 server.use('/api/recipes', recipeRoutes);
-
-// Rutas de Contacto
 server.use('/api/contact', contactRoutes);
+server.use('/api/pass', passwordRoutes);
+ server.use('/api/favorites', favoritesRoutes);
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
